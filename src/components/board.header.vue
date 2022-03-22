5507c42600572b9e6c2cc9190472a5b0b43b732f
BenIsayev <benisayev@gmail.com>
2022-03-22 21:54:49 +0200

    board-header basic start

---
+++ src/components/board.header.vue
@@ -0,0 +1,77 @@
+<template>
+    <section class="board-header">
+        <nav class="board-header-nav">
+            <div class="board-name">
+                <a href>
+                    <h1>{{ board.title }}</h1>
+                </a>
+            </div>
+            <div class="board-starred">
+                <a href>
+                    <span class="star-icon">S</span>
+                </a>
+            </div>
+            <div class="board-privacy">
+                <a href>
+                    <span class="privacy-icon">{{ boardPrivacyIcon }}</span>
+                    <span class="board-privacy-text">Private</span>
+                </a>
+            </div>
+            <div class="board-members">
+                <a v-for="member in board.members" href>
+                    <img :src="member.imgUrl" alt="member img" />
+                </a>
+                <a href>
+                    <span class="invite-icon">I</span>
+                    <span>Invite</span>
+                </a>
+            </div>
+            <div class="board-menu">
+                <a href>
+                    <span class="menu-icon">M</span>
+                    <span>Show menu</span>
+                </a>
+            </div>
+            <div class="board-filter">
+                <a href>
+                    <span class="filter-icon">F</span>
+                    <span>Filter</span>
+                </a>
+            </div>
+        </nav>
+    </section>
+</template>
+
+<script>
+export default {
+    // props: [''],
+    components: {},
+    created() { },
+    data() {
+        return {
+            board: {
+                title: 'TestTest',
+                members: [
+                    {
+                        _id: 'u101',
+                        fullname: 'Tal Tarablus',
+                        imgUrl: 'https://www.google.com',
+                    },
+                    {
+                        _id: 'u102',
+                        fullname: 'Tomer kushtai',
+                        imgUrl: 'https://www.google.com',
+                    },
+                ],
+            }
+        }
+    },
+    methods: {},
+    computed: {
+        boardPrivacyIcon() {
+            return 'Locked'
+        }
+    },
+    unmounted() { },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import homePage from '../views/home.page.vue';
+import boardPage from '../views/board.page.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: homePage,
+    },
+    {
+      path: '/board/:boardId',
+      name: 'board',
+      component: boardPage,
     },
   ],
 });
