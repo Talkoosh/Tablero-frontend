8ada1f7bab690d122be76ae3e6a014ec85b351bc
BenIsayev <benisayev@gmail.com>
2022-03-22 22:35:01 +0200

    board header basic design

--- src/components/board.header.vue
+++ src/components/board.header.vue
@@ -1,42 +1,46 @@
 <template>
     <section class="board-header">
         <nav class="board-header-nav">
-            <div class="board-name">
-                <a href>
-                    <h1>{{ board.title }}</h1>
-                </a>
+            <div class="board-header-left">
+                <div class="board-name">
+                    <a href>
+                        <h1>{{ board.title }}</h1>
+                    </a>
+                </div>
+                <div class="board-starred">
+                    <a href>
+                        <span class="star-icon">S</span>
+                    </a>
+                </div>
+                <div class="board-privacy">
+                    <a href>
+                        <span class="privacy-icon">{{ boardPrivacyIcon }}</span>
+                        <span class="board-privacy-text">Private</span>
+                    </a>
+                </div>
+                <div class="board-members">
+                    <a v-for="member in board.members" href>
+                        <img :src="member.imgUrl" alt="member img" />
+                    </a>
+                    <a href>
+                        <span class="invite-icon">I</span>
+                        <span>Invite</span>
+                    </a>
+                </div>
             </div>
-            <div class="board-starred">
-                <a href>
-                    <span class="star-icon">S</span>
-                </a>
-            </div>
-            <div class="board-privacy">
-                <a href>
-                    <span class="privacy-icon">{{ boardPrivacyIcon }}</span>
-                    <span class="board-privacy-text">Private</span>
-                </a>
-            </div>
-            <div class="board-members">
-                <a v-for="member in board.members" href>
-                    <img :src="member.imgUrl" alt="member img" />
-                </a>
-                <a href>
-                    <span class="invite-icon">I</span>
-                    <span>Invite</span>
-                </a>
-            </div>
-            <div class="board-menu">
-                <a href>
-                    <span class="menu-icon">M</span>
-                    <span>Show menu</span>
-                </a>
-            </div>
-            <div class="board-filter">
-                <a href>
-                    <span class="filter-icon">F</span>
-                    <span>Filter</span>
-                </a>
+            <div class="board-header-right">
+                <div class="board-filter">
+                    <a href>
+                        <span class="filter-icon">F</span>
+                        <span>Filter</span>
+                    </a>
+                </div>
+                <div class="board-menu">
+                    <a href>
+                        <span class="menu-icon">M</span>
+                        <span>Show menu</span>
+                    </a>
+                </div>
             </div>
         </nav>
     </section>
