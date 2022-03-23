e7f25feacc1aa8afea50692770e568bc4ac0750f
Tomer <tomermalkiman8@gmail.com>
2022-03-23 09:21:12 +0200

    header scss

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <router-view />
+<section class="app-container">
+   <main-header/>
+   <router-view />
+</section>
 </template>
 
 
 <script>
-
+import mainHeader from './components/main.header.vue'
 
 export default {
   // props: [''],
-  components: {},
+  components: {
+    mainHeader
+  },
   created() { },
   data() {
     return {}
