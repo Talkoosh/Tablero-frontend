5eec15a3766d53c5f1069b696e478da1c4f050a9
Tomer <tomermalkiman8@gmail.com>
2022-03-23 23:28:50 +0200

    boards page and css

--- src/views/boards.page.vue
+++ src/views/boards.page.vue
@@ -1,5 +1,26 @@
 <template>
-    <section>
+    <section class="boards-page">
+        <div class="boards-page-nav"></div>
+        <div class="starred-boards-container">
+        <div class="all-boards">
+            <div class="starred-boards">
+                <div class="icon">😎</div>
+                <h3 class="starred-header">Starred boards</h3>
+            </div>
+        </div>
+        <div class="starred-boards-list-container">
+            <ul class="starred-boards-list">
+
+            </ul>
+        </div>
+        </div>
+
+        <div class="your-boards">
+
+
+
+        </div>
+
         <router-link v-for="board in boards" :to="'/board/' + board._id">BOARD</router-link>
     </section>
 </template>
