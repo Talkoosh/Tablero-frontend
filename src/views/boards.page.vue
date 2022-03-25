8c2aabb824149c3399b9cc606463e849b3df7213
Tomer <tomermalkiman8@gmail.com>
2022-03-25 12:29:01 +0300

    new-board-card

--- src/views/boards.page.vue
+++ src/views/boards.page.vue
@@ -3,20 +3,15 @@
     <div class="boards-page-nav"></div>
 
     <div class="all-boards">
-
       <div v-if="boards.length" class="starred-boards-container">
         <div class="starred-boards-title">
           <span class="member-icon"></span>
           <h3 class="starred-header">Starred boards</h3>
         </div>
         <div class="starred-boards-list-container">
-          <ul
-            class="starred-boards-list"
-            v-for="board in boards"
-            :key="board._id"
-          >
-            <li class="board-card">
-                <div class="board-card-details"></div>
+          <ul class="starred-boards-list">
+            <li class="board-card" v-for="board in boards" :key="board._id">
+              <div class="board-card-details"></div>
             </li>
           </ul>
         </div>
@@ -28,9 +23,28 @@
           <h3 class="your-boards-header">Your boards</h3>
         </div>
         <div class="your-boards-list-container">
-          <ul class="your-boards-list" v-for="board in boards" :key="board._id">
-            <li class="board-card">
-                <div class="board-card-details"></div>
+          <ul class="your-boards-list">
+            <li class="board-card" v-for="board in boards" :key="board._id">
+              <router-link class="board-link" :to="'/board/' + board._id">
+              <span class="link-block"></span>
+                <div class="board-card-details">
+                    <div class="card-header-name">{{board.title}}</div>
+                    <div class="card-star">
+                        <span class="card-star-container">
+                            <span class="star"></span>
+                        </span>
+                    </div>
+                </div>
+              </router-link>
+
+              <div></div>
+            </li>
+            <li class="create-new-board-card">
+              <div class="new-board-card">
+                  <p>
+                      <span>Create new board</span>
+                  </p>
+              </div>
             </li>
           </ul>
         </div>
