16d3a4212b8c8542e8b77f5593067d33fd19141a
BenIsayev <benisayev@gmail.com>
2022-03-25 18:18:24 +0300

    board nav changing colors and started work on cover display on task preview

--- src/components/task.preview.vue
+++ src/components/task.preview.vue
@@ -1,17 +1,22 @@
 <template>
     <section>
-        <router-link class="task-preview-container" :to="'/board/' + boardId + '/card/' + task._id">
-            <div v-if="task.cover" class="task-cover" :style="cover"></div>
-            <div class="task-preview">
-                <div class="task-labels">
-                    <label-preview v-for="label in task.labelIds" :label="label" :key="label" />
+        <div class="main-task-container">
+            <router-link
+                class="task-preview-container"
+                :to="'/board/' + boardId + '/card/' + task._id"
+            >
+                <div v-if="task.style" class="task-cover" :style="cover"></div>
+                <div class="task-preview" :style="taskCover">
+                    <div class="task-labels">
+                        <label-preview v-for="label in task.labelIds" :label="label" :key="label" />
+                    </div>
+                    <span>{{ task.title }}</span>
+                    <div v-if="task.badges" class="task-badges"></div>
+                    <div v-if="task.members" class="task-members"></div>
+                    <span @click.prevent="openQuickEdit" class="task-quick-edit"></span>
                 </div>
-                <span>{{ task.title }}</span>
-                <div v-if="task.badges" class="task-badges"></div>
-                <div v-if="task.members" class="task-members"></div>
-                <span @click.prevent="openQuickEdit" class="task-quick-edit"></span>
-            </div>
-        </router-link>
+            </router-link>
+        </div>
     </section>
 </template>
 
@@ -33,6 +38,14 @@ export default {
     computed: {
         labelStyle(label) {
             console.log(label)
+        },
+        cover() {
+            if (this.task.style.color) {
+                return `background-color: ${this.task.style.color}; height: 34px`
+            }
+            if (this.task.style.photo) {
+                return `  background-image: url('${this.task.style.photo}'); min-height: 260px; background-size: cover`
+            }
         },
     },
     unmounted() { },
