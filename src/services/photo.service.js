9b590e3dd43e8098d53ee0dfd64dda268b437946
Tal Koosh <artishok.tal@gmail.com>
2022-03-26 22:39:17 +0300

    cover photo color working

--- src/services/photo.service.js
+++ src/services/photo.service.js
@@ -9,7 +9,7 @@ async function loadDefaultPhotos() {
     const storagePhotos = localStorage.getItem(KEY);
     let photos = storagePhotos ? JSON.parse(storagePhotos) : [];
     if (!photos || !photos.length) {
-        const res = await axios.get('https://api.unsplash.com/photos/random/?count=6&&client_id=uFlXoOBB-1QIZ9Qf2FIQEBZjmO6n3HlN6XtOulT3ciA');
+        const res = await axios.get('https://api.unsplash.com/photos/random/?count=12&&client_id=uFlXoOBB-1QIZ9Qf2FIQEBZjmO6n3HlN6XtOulT3ciA');
         res.data.forEach(p => photos.push(p.urls.regular));
         localStorage.setItem(KEY, JSON.stringify(photos))
     }
