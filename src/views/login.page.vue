db107d664529cf31f03ef67be39bda84f42f4450
Tomer <tomermalkiman8@gmail.com>
2022-03-28 17:03:22 +0300

    sign-up page

--- src/views/login.page.vue
+++ src/views/login.page.vue
@@ -8,28 +8,39 @@
     <section class="login-content-container">
       <div class="content-wrapper">
         <div class="login-content">
-          <h1 class="header">Login to Tablero</h1>
+          <h1 v-if="!this.isSignUpPageOpen" class="header"
+          @click="toggleLoginPage"
+          >Login to Tablero</h1>
+          <h1 v-else class="header"
+          @click="toggleLoginPage"
+          >Sign up for your acount</h1>
+
           <div class="login-password-container">
             <div class="email-password">
               <div class="inputs-container">
-                <input type="email" class="email-input" placeholder="Enter email" />
-                <input type="text" class="password-input" placeholder="Enter password" />
+                <input type="email" class="login-input" placeholder="Enter email "
+                v-model="userMail" ref="mailInput" autofocus />
+                <input  type="text" v-model="userPassword"
+                 class="login-input" placeholder="Enter password"  />
               </div>
-              <input type="submit" class="login-submit" value="Log in" />
+              <input v-if="!this.isSignUpPageOpen" type="submit"
+               class="login-submit" value="Log in" />
+               <input v-else type="submit"
+               value="Sign Up" :class="isValidMail" />
             </div>
 
-            <div class="login-methods-container">
+            <div :style="checkTypingSignUp" class="login-methods-container">
               <div class="login-methods-seperator">OR</div>
 
               <div class="login-methods">
-                <div class="google-btn">
+                <div class="method-btn">
                   <span class="google-icon"></span>
-                  <span class="google-text">Continue with google</span>
+                  <span class="text">Continue with Google</span>
                 </div>
 
-                <div class="facebook-btn">
+                <div class="method-btn">
                   <span class="facebook-icon"></span>
-                  <span class="facebook-text">Continue with facebook</span>
+                  <span class="text">Continue with Facebook</span>
                 </div>
               </div>
             </div>
@@ -39,8 +50,13 @@
 
           <ul class="bottom-link">
             <li class="sign-up-link">
-              <router-link :to="'/board/'" class="sign-up"
-                >Sign up for an acount</router-link
+              <div v-if="!this.isSignUpPageOpen"  class="sign-up"
+              @click="toggleLoginPage"
+                >Sign up for an acount</div
+              >
+               <div v-else :to="'/login/'" class="sign-up"
+               @click="toggleLoginPage"
+                >Already have an acount? Log in</div
               >
             </li>
           </ul>
@@ -50,10 +66,10 @@
 
     <div class="background">
         <div class="left-img">
-            <!-- <img src="@assets/img/tabelro-left.svg" alt="" class="left"> -->
+            <svg src="@assets/img/tabelro-left.svg" alt="" class="left"/>
         </div>
         <div class="right-img">
-            <!-- <img src="@assets/img/tabelro-right.svg" alt="" class="right"> -->
+            <svg src="@assets/img/tabelro-right.svg" alt="" class="right"/>
         </div>
     </div>
 
@@ -66,10 +82,35 @@ export default {
   components: {},
   created() {},
   data() {
-    return {};
+    return {
+        isSignUpPageOpen:false,
+        userMail : '',
+        userPassword : '',
+
+    };
   },
-  methods: {},
-  computed: {},
+  methods: {
+      toggleLoginPage(){
+          this.isSignUpPageOpen = !this.isSignUpPageOpen;
+          this.$refs.mailInput.focus();
+      },
+  
+
+
+  },
+  computed: {
+      checkPage(){
+          return (this.isSignUpPageOpen) ? 'display : none' : '';
+      },
+      checkTypingSignUp(){
+          return ((this.userMail || this.userPassword) && this.isSignUpPageOpen)
+           ? 'display : none' : '' ;
+      },
+      isValidMail(){
+          return (this.userMail.includes('@')) ? 'login-submit' : 'unvalid-mail';
+      }
+
+  },
   unmounted() {},
 };
 </script>
