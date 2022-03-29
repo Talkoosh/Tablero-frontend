d44e1d6296c2918ce67d13c3cd8b52806bacec1f
Tomer <tomermalkiman8@gmail.com>
2022-03-29 08:53:08 +0300

    quick edit start

---
+++ src/components/quick.edit.vue
@@ -0,0 +1,33 @@
+ <template>
+  <div class="quick-edit-page">
+      <span class="close-icon"></span>
+
+      <div class="quick-edit-card-container">
+          <div class="quick-edit-task">
+              <div class="task-text"></div>
+              <div class="task"></div>
+          </div>
+          <input type="submit" value="Save">
+          <div class="quick-edit-options"></div>
+      </div>
+  
+  </div>
+</template>
+
+<script>
+// import '' from ''
+export default {
+  name: "",
+  components: {},
+  created() {},
+  data() {
+    return {};
+  },
+  methods: {
+    
+  },
+  computed: {
+   
+  },
+};
+</script>
